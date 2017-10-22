748810cc671ba8e597ea5bf39d4281b208b7c6b9
hardVlad <workvstudy2@gmail.com>
2017-10-23 02:42:57 +0600

    second change

---
+++ npng/devres/NPNG/app/src/main/java/com/example/vlad/npng/ActivityPhotoGallery.java
@@ -0,0 +1,18 @@
+package com.example.vlad.npng;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Vlad on 23.10.2017.
+ */
+
+public class ActivityPhotoGallery extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_gallery);
+    }
+}
