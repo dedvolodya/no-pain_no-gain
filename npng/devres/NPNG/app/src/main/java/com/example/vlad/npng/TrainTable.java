0281cea3e33b473de89a6d8f0001892e8fb1e50f
13th <vimikeva@gmail.com>
2018-02-26 01:03:55 +0200

    lost dir

---
+++ npng/devres/NPNG/app/src/main/java/com/example/vlad/npng/TrainTable.java
@@ -0,0 +1,20 @@
+package com.nopain_nogain.npng;
+
+
+
+public class TrainTable {
+    public String nameTrain;
+    public int id;
+    public String exLst;
+    public int day;
+
+    public TrainTable(){}
+
+    public TrainTable(String nmTrain,int _id, String xLst, int _day) {
+        nameTrain = nmTrain;
+        id = _id;
+        exLst = xLst;
+        day = _day;
+    }
+}
+
